c234e29ec69165fe2b19f3d0481da5487d75c0db
kjm541006 <kjm541113@gmail.com>
2022-09-02 16:56:25 +0900

    form 제출 후 새로고침 방지 및 input 비우기

--- react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -41,20 +41,53 @@ export default function ExpenseForm() {
       return { ...prevState, enteredDate: e.target.value };
     });
   };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+      title: userInput.enteredTitle,
+      amount: userInput.enteredAmount,
+      date: new Date(userInput.enteredDate),
+    };
+
+    setUserInput({
+      enteredTitle: "",
+      enteredAmount: "",
+      enteredDate: "",
+    });
+
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={handleTitleChange} />
+          <input type="text" value={userInput.enteredTitle} onChange={handleTitleChange} required />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={handleAmountChange} />
+          <input
+            type="number"
+            value={userInput.enteredAmount}
+            min="0.01"
+            step="0.01"
+            onChange={handleAmountChange}
+            required
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onChange={handleDateChange} />
+          <input
+            type="date"
+            value={userInput.enteredDate}
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={handleDateChange}
+            required
+          />
         </div>
       </div>
       <div className="new-expense__actions">

a5bcdb7cac9da09171aedf6cad0bd1199f24e5d1
kjm541006 <kjm541113@gmail.com>
2022-09-02 19:17:54 +0900

    stateless 컴포넌트

--- react/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ react/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,14 @@
-import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("changed");
-    console.log(title);
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler}>change title</button>
       </div>
     </Card>
   );

0ccbfd625194f069b0ed4c3113d317f8c8b480c8
kjm541006 <kjm541113@gmail.com>
2022-09-04 18:31:05 +0900

    add expense 나의 구현

--- react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -60,39 +60,62 @@ export default function ExpenseForm(props) {
     });
   };
 
+  const [addNewExpense, setAddNewExpense] = useState(false);
+  const handleNewExpense = () => {
+    setAddNewExpense(true);
+    console.log(addNewExpense);
+  };
+
+  const handleCancel = () => {
+    setAddNewExpense(false);
+  };
+
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input type="text" value={userInput.enteredTitle} onChange={handleTitleChange} required />
-        </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            value={userInput.enteredAmount}
-            min="0.01"
-            step="0.01"
-            onChange={handleAmountChange}
-            required
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            value={userInput.enteredDate}
-            min="2019-01-01"
-            max="2022-12-31"
-            onChange={handleDateChange}
-            required
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+    <div>
+      {!addNewExpense && <button onClick={handleNewExpense}>Add New Expense</button>}
+      {addNewExpense && (
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input
+                type="text"
+                value={userInput.enteredTitle}
+                onChange={handleTitleChange}
+                required
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input
+                type="number"
+                value={userInput.enteredAmount}
+                min="0.01"
+                step="0.01"
+                onChange={handleAmountChange}
+                required
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input
+                type="date"
+                value={userInput.enteredDate}
+                min="2019-01-01"
+                max="2022-12-31"
+                onChange={handleDateChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="new-expense__actions">
+            <button type="button" onClick={handleCancel}>
+              Cancel
+            </button>
+            <button type="submit">Add Expense</button>
+          </div>
+        </form>
+      )}
+    </div>
   );
 }

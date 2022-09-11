5547345c36788c88886da0920bb5a235f7ad537a
kjm541006 <kjm541113@gmail.com>
2022-09-11 23:52:25 +0900

    css 모듈 준비

--- react/01-starting-project/src/components/UI/Button/Button.js
+++ react/01-starting-project/src/components/UI/Button/Button.js
@@ -1,5 +1,7 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
+
+import styles from "./Button.module.css";
 
 const Button = styled.button`
   width: 100%;
